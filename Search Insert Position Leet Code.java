a1edccbbbe328b0562012a96cfdf2b737c36da1e
ISHA CHOPDE <isha.chopde250794@gmail.com>
2017-10-25 11:36:38 -0700

    Search insert position

---
+++ Search Insert Position Leet Code.java
@@ -0,0 +1,20 @@
+//search insert position 
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start=0,end=nums.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                start = mid+1;
+            }
+            else if(target<nums[mid]){
+                end = mid-1;
+            }
+        }
+        return start;
+    }
+}
